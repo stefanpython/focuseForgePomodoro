413e504c7570a778f8ffed8f93482221243ebc5b
stefanpython <stefanhtml@yahoo.com>
2024-03-01 08:08:01 +0200

    Create component files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import "./index.css";
+import Navbar from "./components/Navbar";
+import Clock from "./components/Clock";
+import Tasks from "./components/Tasks";
 
 function App() {
   return (
     <div>
       <h1>Pomodoro</h1>
+      <Navbar />
+      <Clock />
+      <Tasks />
     </div>
   );
 }

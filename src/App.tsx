e1693fca392d6b9bfed687f11806d673681303b4
stefanpython <stefanhtml@yahoo.com>
2024-03-12 16:39:01 +0200

    Add modal from DaisyUI to settings button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,57 @@
 import "./App.css";
 import "./index.css";
-import Navbar from "./components/Navbar";
 import Clock from "./components/Clock";
 import Tasks from "./components/Tasks";
+import { useEffect, useState } from "react";
 
 function App() {
+  // DaisyUI modal
+  const openModal = () => {
+    const modal = document.getElementById("settings");
+    if (modal instanceof HTMLDialogElement) {
+      modal.showModal();
+    } else {
+      console.error("Element with ID 'settings' is not a modal");
+    }
+  };
+
   return (
     <div className="bg-base-200 min-h-screen">
-      <Navbar />
+      {/* Navbar container */}
+      <div className="navbar navbar-center bg-base-200 space-x-20 sm:space-x-44 md:space-x-96 shadow">
+        <div className="navbar-end p-3 pl-12">
+          <img
+            className="w-10 h-10 animate-bounce"
+            src="./logo.png"
+            alt="timer clock"
+          />
+          <span className="text-xl">focuseForge</span>
+        </div>
+
+        <div className="navbar-start p-1">
+          <button
+            className="btn btn-sm btn-outline btn-warning"
+            onClick={openModal}
+          >
+            Settings
+          </button>
+
+          <dialog id="settings" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
+        </div>
+      </div>
 
       <Clock />
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,31 @@
-function Navbar() {
-  return (
-    <div className="navbar navbar-center bg-base-200 space-x-20 sm:space-x-44 md:space-x-96 shadow">
-      <div className="navbar-end p-3 pl-12">
-        <img
-          className="w-10 h-10 animate-bounce"
-          src="./logo.png"
-          alt="timer clock"
-        />
-        <span className="text-xl">focuseForge</span>
-      </div>
+// import React from "react";
 
-      <div className="navbar-start p-1">
-        <button className="btn btn-sm btn-outline btn-warning">Settings</button>
-      </div>
-    </div>
-  );
-}
+// interface NavbarProps {
+//   handleSettings: () => void;
+// }
 
-export default Navbar;
+// const Navbar: React.FC<NavbarProps> = ({ handleSettings }) => {
+//   return (
+//     <div className="navbar navbar-center bg-base-200 space-x-20 sm:space-x-44 md:space-x-96 shadow">
+//       <div className="navbar-end p-3 pl-12">
+//         <img
+//           className="w-10 h-10 animate-bounce"
+//           src="./logo.png"
+//           alt="timer clock"
+//         />
+//         <span className="text-xl">focuseForge</span>
+//       </div>
+
+//       <div className="navbar-start p-1">
+//         <button
+//           className="btn btn-sm btn-outline btn-warning"
+//           onClick={handleSettings}
+//         >
+//           Settings
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
